f503da3b019bdabc1e01d8d18186289876e8b74b
Martin Bogaert <martinbog19@gmail.com>
2026-02-26 16:53:30 -0500

    NHL team logos

--- update_logos.py
+++ update_logos.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import os
 
 
 
@@ -7,14 +8,20 @@ leagues = {
     "nba": "basketball/nba",
     "nfl": "football/nfl",
     "mlb": "baseball/mlb",
+    "nhl": "hockey/nhl",
 }
 
 for league, api_path in leagues.items():
+
+    print(f"Processing {league.upper()}...")
 
     url = f"http://site.api.espn.com/apis/site/v2/sports/{api_path}/teams"
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
+
+    if not os.path.exists(f"graphics/logos/{league}"):
+        os.makedirs(f"graphics/logos/{league}")
 
     with open(f"utils/abbr_{league}.json", "r") as f:
         abbr_dict = json.load(f)
@@ -23,7 +30,7 @@ for league, api_path in leagues.items():
         
         logo_url = tm_data["team"]["logos"][0]["href"]
         team = tm_data["team"]["displayName"]
-        tm = abbr_dict.get(team)
+        tm = abbr_dict[team]
 
         with open(f"graphics/logos/{league}/{tm.upper()}.png", "wb") as f:
             img_resp = requests.get(logo_url)
